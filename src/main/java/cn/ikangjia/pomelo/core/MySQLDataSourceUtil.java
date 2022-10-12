3fa4e4f3b6508596ac9fd5fb57a354d56ab3b13a
kangJia <ikangjia.cn@outlook.com>
2022-10-12 16:25:57 +0800

    [feat] 数据源模块，连通性测试接口实现

--- src/main/java/cn/ikangjia/pomelo/core/MySQLDataSourceUtil.java
+++ src/main/java/cn/ikangjia/pomelo/core/MySQLDataSourceUtil.java
@@ -12,20 +12,20 @@ import java.sql.Statement;
  * @email ikangjia.cn@outlook.com
  * @since 2022/10/9 8:52
  */
-public class DataSourceUtil {
+public class MySQLDataSourceUtil {
     private static final String URL_TEMPLATE = "jdbc:mysql://%s:%s";
 
-    public static Boolean testConnection(DataSourceDO dataSourceDO) {
+    public static Boolean testConnection(DataSourceDO dataSourceDO) throws SQLException {
         return testConnection(dataSourceDO.getHost(), dataSourceDO.getPort(), dataSourceDO.getUsername(), dataSourceDO.getPassword());
     }
 
-    private static Boolean testConnection(String host, String port, String username, String password) {
+    private static Boolean testConnection(String host, String port, String username, String password) throws SQLException {
         String url = String.format(URL_TEMPLATE, host, port);
         try (Connection connection = DriverManager.getConnection(url, username, password);
              Statement statement = connection.createStatement()) {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return statement.execute("select 1");
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
--- src/main/java/cn/ikangjia/pomelo/service/impl/DataSourceServiceImpl.java
+++ src/main/java/cn/ikangjia/pomelo/service/impl/DataSourceServiceImpl.java
@@ -2,7 +2,8 @@ package cn.ikangjia.pomelo.service.impl;
 
 import cn.ikangjia.pomelo.api.query.PageQuery;
 import cn.ikangjia.pomelo.api.vo.DataSourceVO;
-import cn.ikangjia.pomelo.core.DataSourceUtil;
+import cn.ikangjia.pomelo.api.vo.TestConnectionVO;
+import cn.ikangjia.pomelo.core.MySQLDataSourceUtil;
 import cn.ikangjia.pomelo.domain.entity.DataSourceDO;
 import cn.ikangjia.pomelo.domain.mapper.DataSourceMapper;
 import cn.ikangjia.pomelo.service.DataSourceService;
@@ -11,7 +12,9 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -71,9 +74,22 @@ public class DataSourceServiceImpl implements DataSourceService {
     }
 
     @Override
-    public Boolean testConnection(DataSourceDO dataSourceDO) {
-        Long id = dataSourceDO.getId();
-        DataSourceDO sourceDO = dataSourceMapper.selectById(1L);
-        return DataSourceUtil.testConnection(sourceDO);
+    public TestConnectionVO testConnection(DataSourceDO dataSourceDO) {
+        TestConnectionVO result = new TestConnectionVO();
+        switch (dataSourceDO.getType()) {
+            case "1":
+                try {
+                    Boolean connected = MySQLDataSourceUtil.testConnection(dataSourceDO);
+                    result.setConnected(connected);
+                    result.setMsg("可成功连接");
+                } catch (SQLException e) {
+                    result.setConnected(false);
+                    result.setMsg(e.getMessage());
+                }
+                return result;
+            case "2":
+                throw new RuntimeException("暂不支持的数据库类型");
+        };
+        return result;
     }
 }
