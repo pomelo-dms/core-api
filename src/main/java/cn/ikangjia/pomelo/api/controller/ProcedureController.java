77a936a9475966305b0bb2162f93b00ebe0d3031
kangJia <ikangjia.cn@outlook.com>
2022-10-27 17:44:13 +0800

    [feat] 视图查询接口开发

---
+++ src/main/java/cn/ikangjia/pomelo/api/controller/ProcedureController.java
@@ -0,0 +1,16 @@
+package cn.ikangjia.pomelo.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 存储过程控制器
+ *
+ * @author kangJia
+ * @email ikangjia.cn@outlook.com
+ * @since 2022/10/27 13:48
+ */
+@RestController
+@RequestMapping("/api/v1/procedure")
+public class ProcedureController {
+}
